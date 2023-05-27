f0dc601ccd6185581d95742e420090bbc0a4d02b
Willy Alfianto <willy.alfianto@ishangry.com>
2023-05-27 19:11:21 +0700

    demo click action

--- src/app/explore/page.js
+++ src/app/explore/page.js
@@ -1,27 +1,30 @@
-'use client'
-import React from "react";
-import GoogleMapReact from 'google-map-react';
-import Image from 'next/image'
-import grugIcon from './grug.png'
-import thetanIcon from './thetan.jpg'
-import locations from './locations.json'
+"use client";
+import React, { useMemo, useState } from "react";
+import GoogleMapReact from "google-map-react";
+import Image from "next/image";
+import grugIcon from "./grug.png";
+import thetanIcon from "./thetan.jpg";
+import locations from "./locations.json";
 
-const AnyReactComponent = ({ text, icon }) => (
-  <div style={{ 
-    backgroundColor: 'white', 
-    border: '1px solid', 
-    textAlign: 'center', 
-    borderRadius: '10px',
-    display: 'inline-block',
-    padding: '10px'
-  }}>
-    <p style={{ margin: '10px', fontSize: '18px' }}>{text}</p>
-    <Image 
-      src={icon} 
-      alt={text} 
-      width={100}
-      height={100}
-    />
+const AnyReactComponent = ({ text, icon, active, onClick }) => (
+  <div
+    style={{
+      backgroundColor: "white",
+      border: "1px solid",
+      textAlign: "center",
+      borderRadius: "10px",
+      position: "relative",
+      display: "inline-block",
+      padding: active ? 8 : 4,
+    }}
+    onClick={onClick}
+  >
+    {active && (
+      <p style={{ margin: "10px", fontSize: "18px", color: "black" }}>{text}</p>
+    )}
+    <div style={{ width: active ? 100 : 24, aspectRatio: 1, position: "relative" }}>
+      <Image src={icon} alt={text} fill />
+    </div>
   </div>
 );
 
@@ -30,17 +33,19 @@ const handleApiLoaded = (map, maps) => {
 };
 
 export default function SimpleMap() {
+  const [selectedMarker, setSelectedMarker] = useState(-1);
+
   const defaultProps = {
     center: {
       lat: 59.955413,
-      lng: 30.337844
+      lng: 30.337844,
     },
-    zoom: 13
+    zoom: 13,
   };
 
   const renderMarkers = () => {
     return locations.map((location, index) => {
-      const icon = location.type === 'Resto' ? thetanIcon : grugIcon;
+      const icon = location.type === "Resto" ? thetanIcon : grugIcon;
       return (
         <AnyReactComponent
           key={index}
@@ -48,13 +53,15 @@ export default function SimpleMap() {
           lng={location.lng}
           text={location.name}
           icon={icon}
+          active={selectedMarker == index}
+          onClick={() => setSelectedMarker(index)}
         />
-      )
-    })
+      );
+    });
   }
 
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
+    <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY }}
         defaultCenter={defaultProps.center}
