d65c3c1aa4b73e23058c2330b2d5339782209320
syn22 <syn22.eth@gmail.com>
2023-06-07 18:41:16 +0700

    wrong file name

--- src/app/next,config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    images: {
-      domains: ['maps.google.com'],  // replace 'example.com' with the domain that hosts your images
-    },
-  }
---
+++ src/app/next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    images: {
+      domains: ['maps.google.com', 'maps.googleapis.com'],  // replace 'example.com' with the domain that hosts your images
+    },
+  }
